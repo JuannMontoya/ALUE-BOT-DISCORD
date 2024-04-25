a3e4724a256ddf7098c85987b0f5dc1f58211bf7
JuannMontoya <juamontoyara@unal.edu.co>
2024-04-25 15:26:06 -0500

    initial config

---
+++ CommandRegister.js
@@ -0,0 +1,24 @@
+
+//Register a slash command against the Discord API:
+
+import { REST, Routes } from 'discord.js';
+import config from './config.json';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
---
+++ config.js.example
@@ -0,0 +1,8 @@
+{
+  "TOKEN": "",
+  "MAX_PLAYLIST_SIZE": 10,
+  "PRUNING": false,
+  "LOCALE": "en",
+  "STAY_TIME": 30,
+  "DEFAULT_VOLUME": 100
+}
